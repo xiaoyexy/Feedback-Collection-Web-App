525e7f8054e1677b98a614719eef1e31c3e70fd8
Ye Xiao <xiaoye.xy127@gmail.com>
2021-06-21 11:44:06 -0700

    initial heroku

---
+++ readme.md
@@ -0,0 +1,3 @@
+server repo in https://git.heroku.com/shielded-oasis-79598.git
+
+heroku app https://shielded-oasis-79598.herokuapp.com
---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express'); // CommonJS Module
+// import express from 'express'; // ES6 Module
+const app = express();
+
+app.get('/', (request, result) => {
+    result.send({ "hi": 'there' });
+
+});
+
+const PORT = process.env.PORT || 5000 // Heroku App port or local machine port
+app.listen(PORT);
